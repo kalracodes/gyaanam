7ca7a3389722bbe1b847032093e97a92784a8794
aryan <aryankalra02@gmail.com>
2023-01-20 01:38:52 +0530

    Homepage done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {link} from "react-router-dom";
 import { Navbar } from "../components/navbar";
 import "../styles/homepage.css";
 import img1 from "../assests/homepage/img1.jpeg";
@@ -6,7 +7,10 @@ import img2 from "../assests/homepage/img2.svg";
 import img3 from "../assests/homepage/img3.jpeg";
 import img4 from "../assests/homepage/img4.svg";
 import img5 from "../assests/homepage/img5.jpeg";
-import img6 from "../assests/homepage/img6.svg"
+import img6 from "../assests/homepage/img6.svg";
+import feather from "../assests/homepage/feather.svg";
+import eye from "../assests/homepage/eye.svg";
+import sun from "../assests/homepage/sun.svg";
 export function HomePage() {
 	return (
 		<>
@@ -76,7 +80,9 @@ export function HomePage() {
 								Accessible to all
 							</div>
 							<div className="home-services-body-para">
-								Gyaanam Educare Tutorials learning system is designed to make learning easy and accessible for all students.
+								Gyaanam Educare Tutorials learning system is
+								designed to make learning easy and accessible
+								for all students.
 							</div>
 						</div>
 					</div>
@@ -84,10 +90,12 @@ export function HomePage() {
 						<img src={img4} height="150px" alt="" />
 						<div className="home-services-content-body home-services-content-common-body">
 							<div className="home-services-body-headers">
-							Student-friendly
+								Student-friendly
 							</div>
 							<div className="home-services-body-para">
-							We have a dedicated team of educators who are constantly working to improve our platform and make it more student-friendly.
+								We have a dedicated team of educators who are
+								constantly working to improve our platform and
+								make it more student-friendly.
 							</div>
 						</div>
 					</div>
@@ -95,15 +103,80 @@ export function HomePage() {
 						<img src={img6} height="150px" alt="" />
 						<div className="home-services-content-body home-services-content-common-body">
 							<div className="home-services-body-headers">
-							Better support 
+								Better support
 							</div>
 							<div className="home-services-body-para">
-							You will be playing a vital role in helping us understand the challenges that students face and how we can better support you.
+								You will be playing a vital role in helping us
+								understand the challenges that students face and
+								how we can better support you.
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+			<div className="home-features">
+				<div className="home-feature1 features">
+					<div className="home-feature1-content">
+						<div className="home-feature1-para">
+							Every student's
+							<br /> Education companion
+						</div>
+						<div className="home-feature1-points">
+							<ul>
+								<li>
+									<img src={feather} alt="" />
+									<div className="home-feature1-paragr">
+										Understanding the problem
+									</div>
+								</li>
+								<li>
+									<img src={eye} alt="" />
+									<div className="home-feature1-paragr">
+										Unlocking their potential
+									</div>
+								</li>
+								<li>
+									<img src={sun} alt="" />
+									<div className="home-feature1-paragr">
+										Unfold your future
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="home-feature-img1"></div>
+				</div>
+				{/* <div className="home-feature2 features">
+					<div className="home-feature2-content">
+						<div className="home-feature2-para">
+							Every student's <br/>Education companion
+							<div className="home-feature2-points">
+								<ul>
+									<li>
+										<img src={feather} alt="" />
+									</li>
+									<li>
+										<img src={eye} alt="" />
+									</li>
+									<li>
+										<img src={sun} alt="" />
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div className="home-feature-img2"></div>
+					</div>
+				</div> */}
+			</div>
+			<div className="home-survey">
+				Take Survey CTA
+				<div className="home-survey-button">
+					<button>Start now</button>
+				</div>
+			</div>
+			<footer>
+			All rights reserved @Gyaanam2023
+			</footer>
 		</>
 	);
 }
